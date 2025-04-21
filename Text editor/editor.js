455588b3a56edbc95bd7b4eb2898ab231d968d55
LynchHack <izidor.zadravec@druga.si>
2025-04-21 13:33:48 +0200

    Add files via upload

--- Text editor/editor.js
+++ Text editor/editor.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     let optionButton = $(".option-button");
     let advancedOptionButton = $(".adv-option-button");
     let fontName = $("#fontName");
@@ -8,88 +8,140 @@ $(document).ready(function(){
     let alignButton = $(".align");
     let spacingButton = $(".spacing");
     let formatButton = $(".format");
-    let scriptButton = $(".script")
-
+    let scriptButton = $(".script");
 
     let fontList = [
-        "Arial",
-        "Verdana",
-        "Times New Roman",
-        "Garamond",
-        "Georgia",
-        "Courier New",
-        "cursive",
+        "Arial", "Verdana", "Times New Roman",
+        "Garamond", "Georgia", "Courier New", "cursive"
     ];
 
-    const initializer = () =>{
+    // Initialize editor
+    const initializer = () => {
         highlighter(alignButton, true);
         highlighter(scriptButton, true);
         highlighter(formatButton, true);
 
-        fontList.map((value) => {
-            let option = $("<option></option>").val(value).html(value);
-            fontName.append(option);
+        // Populate font list
+        fontList.forEach((font) => {
+            fontName.append($("<option></option>").val(font).text(font));
         });
 
-        for(let i = 1; i<= 7; i++){
-            let option = $("<option></option>").val(i).html(i);
-            fontSize.append(option);
+        // Populate font sizes
+        for (let i = 1; i <= 7; i++) {
+            fontSize.append($("<option></option>").val(i).text(i));
         }
 
-        fontSize.val(3);
-    }
+        fontSize.val(3); // Default font size
+    };
 
-    const modifyText = (command, defaultUi, value) =>{
+    // Function to modify text properties
+    const modifyText = (command, defaultUi = false, value = null) => {
         document.execCommand(command, defaultUi, value);
-    }
+    };
 
+    // Text formatting buttons
+    formatButton.on("click", function () {
+        $(this).toggleClass("active");
+        modifyText($(this).attr("id"));
+    });
+
+    // Alignment buttons
+    alignButton.on("click", function () {
+        modifyText($(this).attr("id"));
+    });
+
+    // Subscript/Superscript buttons
+    scriptButton.on("click", function () {
+        modifyText($(this).attr("id"));
+    });
+
+    // Text & Background Color
+    $("#colorPicker").on("input", function () {
+        modifyText("foreColor", false, $(this).val());
+    });
+
+    $("#bgColorPicker").on("input", function () {
+        modifyText("hiliteColor", false, $(this).val());
+    });
+
+    // Insert unordered list
     $("#insertUnorderedList").on("click", function () {
         modifyText("insertUnorderedList", false, null);
         $(this).toggleClass("active");
     });
-    
+
+    // Insert ordered list
     $("#insertOrderedList").on("click", function () {
         modifyText("insertOrderedList", false, null);
         $(this).toggleClass("active");
     });
-    
-    optionButton.on("click", function() {
-        $(this).toggleClass("active"); // Toggle active class
-        modifyText($(this).attr("id"), false, $(this).val());
-    });    
-    advancedOptionButton.on("change", function(){
+
+    // Hyperlink
+    linkButton.on("click", function () {
+        let url = prompt("Enter a URL:");
+        if (url) {
+            if (!/^http/i.test(url)) {
+                url = "http://" + url;
+            }
+            modifyText("createLink", false, url);
+        }
+    });
+
+    // Save & Load functionality
+    $("#save").on("click", function () {
+        localStorage.setItem("savedText", writingArea.html());
+        alert("Text saved!");
+    });
+
+    $("#load").on("click", function () {
+        let savedText = localStorage.getItem("savedText");
+        if (savedText) {
+            writingArea.html(savedText);
+            alert("Text loaded!");
+        } else {
+            alert("No saved text found.");
+        }
+    });
+
+    // Apply font change with better browser compatibility
+    fontName.on("change", function () {
+        let selectedFont = $(this).val();
+        document.execCommand("fontName", false, selectedFont);
+        document.execCommand("insertHTML", false, `<span style="font-family: ${selectedFont};">${window.getSelection()}</span>`);
+    });
+
+    // Apply font size
+    fontSize.on("change", function () {
+        let sizeValue = $(this).val();
+        document.execCommand("fontSize", false, sizeValue);
+    });
+
+    // Advanced text options
+    optionButton.on("click", function () {
+        $(this).toggleClass("active");
         modifyText($(this).attr("id"), false, $(this).val());
     });
 
-    linkButton.on("click", function(){
-    let userLink = prompt("Enter a URL");
-    if(/http/i.test(userLink)){
-        modifyText(linkButton.attr("id"), false, userLink);
-    }else{
-        userLink = "http://" + userLink
-        modifyText(linkButton.attr("id"), false, userLink);
-    }
+    advancedOptionButton.on("change", function () {
+        modifyText($(this).attr("id"), false, $(this).val());
     });
 
+    // Highlighter function to manage button active states
     const highlighter = (className, needsRemoval) => {
-        className.on("click", function(){
-            if(needsRemoval){
-                let alreadyActive = false;
-                if($(this).hasClass("active")){
-                    alreadyActive = true;
-                }
-
+        className.on("click", function () {
+            if (needsRemoval) {
+                let alreadyActive = $(this).hasClass("active");
                 highlighterRemover(className);
-                if(!alreadyActive){
+                if (!alreadyActive) {
                     $(this).addClass("active");
                 }
             }
         });
-    }
+    };
 
     const highlighterRemover = (className) => {
         className.removeClass("active");
     };
 
-    window.onload = initializer;
+    initializer();
 });
